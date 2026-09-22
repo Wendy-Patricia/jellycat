95b72bc6000a1405a2cba86e9091fd752d613b1e
Wendy-Patricia <wendypatriciam2018@gmail.com>
2026-09-22 15:18:18 +0200

    feat: add health endpoints

--- src/main/java/com/example/jellycat/ChatApplication.java
+++ src/main/java/com/example/jellycat/ChatApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JellycatApplication {
+public class ChatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JellycatApplication.class, args);
+        SpringApplication.run(ChatApplication.class, args);
     }
-
 }
