274ec0be9a42511b2810a999da4b490105c0db4f
Wendy-Patricia <wendypatriciam2018@gmail.com>
2026-09-22 10:54:35 +0200

    Initial commit

---
+++ src/main/java/com/example/jellycat/JellycatApplication.java
@@ -0,0 +1,13 @@
+package com.example.jellycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellycatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JellycatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jellycat/JellycatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jellycat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellycatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
